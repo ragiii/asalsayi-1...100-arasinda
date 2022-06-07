5190ea0177e881d1d22d1e1973492f29285f2aca
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-06-07 20:31:58 +0300

    Update main.java

--- main.java
+++ main.java
@@ -2,9 +2,9 @@ public class main {
     public static void main(String[] args) {
 
         String primeNumbers = "";
-        int counter = 0;
         
         for (int i = 1; i <= 100; i++) {
+             int counter = 0;
             for (int j = i; j >= 1; j--) {
                 if (i % j == 0) {
                     counter ++;
